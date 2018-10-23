eb5defbec7b222892781725cc046188618d6d987
Michael Wong <michael1998@hotmail.ca>
2018-10-23 14:13:14 -0700

    Added server bullet firing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,30 @@ using UnityEngine.Networking;
 public class PlayerController : NetworkBehaviour
 {
     public GameObject playerCamera;
+    public float xSensitivity;
+    private float yaw = 0.0f;
+
     public float xSpeed = 3.0f;
     public float ySpeed = 3.0f;
 
-    public float xSensitivity;
-    private float yaw = 0.0f;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public int bulletSpeed = 6;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            playerCamera.SetActive(false);
+            return;
+        }
+    }
+
     void Update()
     {
     
         if (!isLocalPlayer)
         {
-            playerCamera.SetActive(false);
             return;
         }
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * xSpeed;
@@ -22,6 +35,11 @@ public class PlayerController : NetworkBehaviour
         transform.Translate(x, 0, 0);
         transform.Translate(0, 0, z);
         transform.rotation = Quaternion.Euler(0, yaw, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
     }
 
     public override void OnStartLocalPlayer()
@@ -32,5 +50,16 @@ public class PlayerController : NetworkBehaviour
         }
 
         GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    [Command]
+    private void CmdFire()
+    {
+        var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 2.0f);
     }
 }
